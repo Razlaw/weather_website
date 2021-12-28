0e10f381592ce184d0a14289ff8e0ae4b2d42a4a
Razlaw <jan.razlaw@gmx.de>
2021-12-28 14:36:48 +0100

    Reformat code - intendation

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -1,12 +1,12 @@
 import "./currentWeather.scss";
-import { Card } from "semantic-ui-react";
+import {Card} from "semantic-ui-react";
 
-export default function CurrentWeather({ weatherData }) {
-  return (
-    <Card>
-      <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
-      </Card.Content>
-    </Card>
-  );
+export default function CurrentWeather({weatherData}) {
+    return (
+        <Card>
+            <Card.Content>
+                <Card.Header className="header">{weatherData.name}</Card.Header>
+            </Card.Content>
+        </Card>
+    );
 }
