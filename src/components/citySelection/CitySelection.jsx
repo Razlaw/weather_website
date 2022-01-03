b1168921ee50f8c7ca21ece425bf7b81fbe1862c
Razlaw <jan.razlaw@gmx.de>
2022-01-03 22:40:53 +0100

    Replace text in submit button with icon of magnifying glass

--- src/components/citySelection/CitySelection.jsx
+++ src/components/citySelection/CitySelection.jsx
@@ -1,16 +1,22 @@
 import "./citySelection.scss";
+import { ReactComponent as MagGlass } from '../../assets/magnifying_glass.svg';
 
 export default function CitySelection({cityName, updateCityName, reloadWeather}) {
     return (
         <div className="wrapper">
             <form onSubmit={reloadWeather}>
-                <input
-                    onChange={(e) => updateCityName(e.target.value)}
-                    placeholder={cityName === "" ? "City" : cityName}
-                    type="text"
-                    id="cityNameInput"
-                />
-                <button type="submit">Submit</button>
+                <div className="formComponents">
+                    <input
+                        onChange={(e) => updateCityName(e.target.value)}
+                        placeholder={cityName === "" ? "City" : cityName}
+                        type="text"
+                        id="cityNameInput"
+                        autoFocus
+                    />
+                    <button type="submit">
+                        <MagGlass className="mag_glass"/>
+                    </button>
+                </div>
             </form>
         </div>
     );
