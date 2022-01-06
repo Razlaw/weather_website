feba300648a0620c03cb9984ca1b6afc20fc4f3e
Razlaw <jan.razlaw@gmx.de>
2022-01-06 18:45:47 +0100

    Refactoring - Clean up in CitySearchBar

--- src/components/citySearchBar/CitySearchBar.jsx
+++ src/components/citySearchBar/CitySearchBar.jsx
@@ -1,23 +1,21 @@
 import "./citySearchBar.scss";
-import {ReactComponent as MagGlass} from '../../assets/magnifying_glass.svg';
+import {ReactComponent as MagnifyingGlassIcon} from '../../assets/magnifying_glass.svg';
 
 export default function CitySearchBar({cityName, updateCityName, reloadWeather}) {
     return (
-        <div className="wrapper">
+        <div className="citySearchBar">
             <form onSubmit={reloadWeather}>
                 <div className="formComponents">
-                    <div className="formComponentsWrapper">
-                        <input
-                            onChange={(e) => updateCityName(e.target.value)}
-                            placeholder={cityName === "" ? "City" : cityName}
-                            type="text"
-                            id="cityNameInput"
-                            autoFocus
-                        />
-                        <button type="submit">
-                            <MagGlass className="mag_glass"/>
-                        </button>
-                    </div>
+                    <input
+                        onChange={(e) => updateCityName(e.target.value)}
+                        placeholder={cityName === "" ? "City" : cityName}
+                        type="text"
+                        id="cityNameInput"
+                        autoFocus
+                    />
+                    <button type="submit">
+                        <MagnifyingGlassIcon className="magnifyingGlassIcon"/>
+                    </button>
                 </div>
             </form>
         </div>
