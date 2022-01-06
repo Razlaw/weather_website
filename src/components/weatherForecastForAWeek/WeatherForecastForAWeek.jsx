4f1a52e530476685977a3547125123bc7a310718
Razlaw <jan.razlaw@gmx.de>
2022-01-06 17:55:19 +0100

    Add component hull for the weather forecast of the week

---
+++ src/components/weatherForecastForAWeek/WeatherForecastForAWeek.jsx
@@ -0,0 +1,10 @@
+import "./weatherForecastForAWeek.scss";
+import React, {useState} from "react";
+
+export default function WeatherForecastForAWeek({cityName, weatherData}) {
+    return (
+        <div className="weatherForecastForAWeek">
+            Forecast for a week
+        </div>
+    );
+}
