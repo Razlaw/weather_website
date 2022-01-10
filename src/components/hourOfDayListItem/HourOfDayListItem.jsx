397a9fb87f745711209b87adca2ff8ea5537657f
Razlaw <jan.razlaw@gmx.de>
2022-01-10 21:23:46 +0100

    Remove unused setter function

--- src/components/hourOfDayListItem/HourOfDayListItem.jsx
+++ src/components/hourOfDayListItem/HourOfDayListItem.jsx
@@ -1,10 +1,9 @@
 import "./hourOfDayListItem.scss"
 
-export default function HourOfDayListItem({time, active, setSelected}) {
+export default function HourOfDayListItem({time, active}) {
     return (
         <li
             className={active ? "hourOfDayListItem active" : "hourOfDayListItem"}
-            onClick={() => setSelected(time)}
         >
             {time}:00
         </li>
