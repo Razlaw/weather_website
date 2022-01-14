9f37e49486759a4ee4a104c01450c1c0398368f5
Razlaw <jan.razlaw@gmx.de>
2022-01-14 17:51:37 +0100

    Add two additional plots and switch by pressing buttons

--- src/components/weatherForecastForADay/WeatherForecastForADay.jsx
+++ src/components/weatherForecastForADay/WeatherForecastForADay.jsx
@@ -1,11 +1,43 @@
 import "./weatherForecastForADay.scss";
 import HourOfDayListItem from "../hourOfDayListItem/HourOfDayListItem";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PlotForADay from "../plotForADay/PlotForADay";
 
 export default function WeatherForecastForADay({dayToDisplay, weatherData}) {
     const today = new Date();
     const currentHour = today.getHours();
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [temperatures, setTemperatures] = useState( [{"temp": 0, "tempForPlot": 1}]);
+
+    const handleClick = (direction)=>{
+        direction === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3 - 1) :
+            setCurrentSlide(currentSlide < 3 - 1 ? currentSlide + 1 : 0);
+    };
+
+    // Workaround: recharts draws bars with positive and negative values into different directions.
+    //  I didn't find a way to change the behaviour to draw all bars into one direction - setting the bottom
+    //  value of the bars as its called in matplotlib. This workaround shifts all values into a range starting
+    //  from the value 1 if at least one element is negative. The shifted values as used for plotting while the
+    //  original values are used for the labels of the bars.
+    useEffect(() => {
+        let roundedTemperatureArray = new Array(0);
+        for(let i = 0; i < weatherData.length; i++) {
+            let roundedTemperature = Math.round(weatherData[i]["temp"]);
+            roundedTemperatureArray.push(roundedTemperature);
+        }
+
+        let lowestTemperature = Math.min(...roundedTemperatureArray);
+        let temperatureArrayForPlot = roundedTemperatureArray.map( function(value) {
+            return value - (lowestTemperature - 1);
+        } );
+
+        let temperatureArray = new Array(0);
+        for(let i = 0; i < weatherData.length; i++) {
+            temperatureArray[i] = {"temp": roundedTemperatureArray[i], "tempForPlot": temperatureArrayForPlot[i]};
+        }
+        setTemperatures(temperatureArray);
+    }, [weatherData]);
 
     return (
         <div className="weatherForecastForADay">
@@ -28,7 +60,19 @@ export default function WeatherForecastForADay({dayToDisplay, weatherData}) {
                         </ul>
                     </div>
                     <div className="forecastPlot">
-                        <PlotForADay currentHour={currentHour} weatherData={weatherData}/>
+                        <h1 className="left" onClick={() => handleClick("left")}>L</h1>
+                        <div className="plotContainer">
+                            <div className="tempPlot" style={{transform: `rotateY(${currentSlide * 0.25}turn)`}} >
+                                <PlotForADay currentHour={currentHour} weatherData={temperatures} unit={"°C"}/>
+                            </div>
+                            <div className="windPlot" style={{transform: `rotateY(${(currentSlide-2) * 0.25}turn)`}}>
+                                <PlotForADay currentHour={currentHour} weatherData={temperatures} unit={"m/s"}/>
+                            </div>
+                            <div className="rainPlot" style={{transform: `rotateY(${(currentSlide-1) * 0.25}turn)`}}>
+                                <PlotForADay currentHour={currentHour} weatherData={temperatures} unit={"mm"}/>
+                            </div>
+                        </div>
+                        <h1 className="right" onClick={() => handleClick("right")}>R</h1>
                     </div>
                 </div>
             </div>
