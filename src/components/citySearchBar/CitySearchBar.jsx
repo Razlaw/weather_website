a66c7a7b3732391b03cad1a08c6f8d864f144542
Razlaw <jan.razlaw@gmx.de>
2022-01-31 16:26:36 +0100

    Hide keyboard when a city name is submitted using the enter key

--- src/components/citySearchBar/CitySearchBar.jsx
+++ src/components/citySearchBar/CitySearchBar.jsx
@@ -2,11 +2,18 @@ import "./citySearchBar.scss";
 import {ReactComponent as MagnifyingGlassIcon} from '../../assets/magnifying_glass.svg';
 
 export default function CitySearchBar({cityName, updateCityName, reloadWeather}) {
+    function hideKeyboardOnEnter(e) {
+        if (e.key === 'Enter') {
+            e.target.blur();
+        }
+    }
+
     return (
         <div className="citySearchBar">
             <form onSubmit={reloadWeather}>
                 <div className="formComponents">
                     <input
+                        onKeyUp={hideKeyboardOnEnter}
                         onChange={(e) => updateCityName(e.target.value)}
                         placeholder={cityName === "" ? "City" : cityName}
                         type="text"
